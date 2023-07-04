5e110799ac22c424800737f0bf11521abcd87a56
dexter ramos <dexter.ramos@epon.ph>
2023-07-04 08:56:25 +0800

    test

--- Sources/PackageDemo/PackageDemo.swift
+++ Sources/PackageDemo/PackageDemo.swift
@@ -1,6 +1,20 @@
-public struct PackageDemo {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public struct PackageDemo {
+    static func objectFromJSON<T: Codable>(_ filename: String) -> T? {
+        
+        let decoder = JSONDecoder()
+        
+        if let path = Bundle.main.path(forResource: filename, ofType: "json"),
+           let jsonData = NSData(contentsOfFile: path)
+        {
+            let jsonObject = try! decoder.decode(T.self, from: jsonData as Data)
+            return jsonObject
+        } else {
+            return nil
+        }
     }
 }
+
+
+
